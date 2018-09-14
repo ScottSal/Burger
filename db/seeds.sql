3f278746d658b11e561c03f92600e1695cbe939b
Scott Saloom <scottsaloom@gmail.com>
2018-09-14 11:28:36 -0500

    Updated DB

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheeseburger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Baconburger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Salmonburger", FALSE);
