bb1a3400374f5dbda7e95f87c111e14f0186aa0c
Scott Saloom <scottsaloom@gmail.com>
2018-09-14 11:44:07 -0500

    Updated Code

---
+++ config/connection.js
@@ -0,0 +1,25 @@
+//this file houses all mysql connection data
+
+var mysql = require('mysql');
+
+if (process.env.JAWSDB_URL){
+    var connection = mysql.createConnection(process.env.JAWSDB_URL);
+  } else {
+    var connection = mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: 'burgers_db'
+  });
+};
+ 
+connection.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+   
+    console.log('connected as id ' + connection.threadId);
+  });
+
+  module.exports = connection;
